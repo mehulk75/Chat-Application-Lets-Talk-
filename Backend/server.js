c28b8be32d7d37e7a7e76714279cd23f1587d381
mehulk75 <mehulkumar_mc21a14_75@dtu.ac.in>
2024-10-04 00:14:10 +0530

    auth login

--- Backend/server.js
+++ Backend/server.js
@@ -33,3 +33,16 @@ app.use("/api/auth", authRoutes)
 app.listen(3000, () => {
   console.log("server is running " + PORT);
 });
+
+
+// error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || "Internal Server Error"
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
