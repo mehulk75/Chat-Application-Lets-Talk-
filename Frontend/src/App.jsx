67d2dd8d93d2980bc3ba123b6182159e9dcdfb9d
mehulk75 <mehulkumar_mc21a14_75@dtu.ac.in>
2024-10-14 20:59:05 +0530

    Login Button added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
-    <button className="btn btn-primary">Button</button>
-  )
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
