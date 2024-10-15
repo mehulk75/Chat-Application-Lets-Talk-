064fd4f960628a934a2bf2e0cd5086de429c1010
mehulk75 <mehulkumar_mc21a14_75@dtu.ac.in>
2024-10-15 17:25:28 +0530

    Messages UI

--- Frontend/src/component/messages/MessageContainer.jsx
+++ Frontend/src/component/messages/MessageContainer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import MessageInput from './MessageInput'
+import Messages from './messages'
 
 const MessageContainer = () => {
   return (
-    <div>MessageContainer</div>
+    <div className="md:min-w-[450px] flex flex-col">
+
+    <>
+          {/* Header */}
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text">To: </span>
+            <span className="text-gray-900 font-bold"> JOn Snow
+            </span>
+          </div>
+
+         <Messages/>
+
+         <MessageInput/>
+        </>
+
+    </div>
   )
 }
 
