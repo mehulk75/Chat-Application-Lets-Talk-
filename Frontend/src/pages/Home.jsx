779e4e724751c953d1205ae8933a676e85d0d13e
mehulk75 <mehulkumar_mc21a14_75@dtu.ac.in>
2024-10-15 16:45:54 +0530

    added basic sidebar in homepage

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from "react"
+import Sidebar from "../component/sidebar/Sidebar"
+import MessageContainer from "../component/messages/MessageContainer"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-200">
+      <Sidebar />
+
+      <MessageContainer />
+    </div>
   )
 }
 
