0efe4ef280bcfce72ac86a191cddc154bfcbe9ee
mehulk75 <mehulkumar_mc21a14_75@dtu.ac.in>
2024-10-16 17:34:34 +0530

    get conversations functionality i.e sidebar icons of friends

--- Frontend/src/component/sidebar/Conversations.jsx
+++ Frontend/src/component/sidebar/Conversations.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import Conversation from './Conversation'
+import React from "react"
+import Conversation from "./Conversation"
+import useGetConversations from "../../hooks/useGetConversations"
 
-const Conversations = () => {
+const Coversations = () => {
+  const { loading, conversations } = useGetConversations()
+  // console.log(conversations)
+
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
+      {conversations.map((conversation, index) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          lastIndex={index === conversation.length - 1}
+        />
+      ))}
+
+      {loading ? <span className="loading loading-spinner"></span> : null}
     </div>
   )
 }
 
-export default Conversations
+export default Coversations
