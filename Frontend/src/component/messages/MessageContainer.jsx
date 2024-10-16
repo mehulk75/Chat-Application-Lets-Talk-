c4eae8e110cf469f3c73dd765b5dd2175719830b
mehulk75 <mehulkumar_mc21a14_75@dtu.ac.in>
2024-10-16 18:40:44 +0530

    get message functionality completed

--- Frontend/src/component/messages/MessageContainer.jsx
+++ Frontend/src/component/messages/MessageContainer.jsx
@@ -10,7 +10,7 @@ const MessageContainer = () => {
 
   useEffect(() => {
     return () => setSelectedConversation(null)
-  }, [])
+  }, [setSelectedConversation])
 
   return (
     <div className="md:min-w-[450px] flex flex-col">
